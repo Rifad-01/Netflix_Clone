8208b2d8c46119c225bd6a85df35a8f1d48704c2
Mohammed Rifad <rifadferoke01@gmail.com>
2025-06-12 23:55:26 +0530

    initial commit

---
+++ lib/widgets/bottom_nav_bar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        bottomNavigationBar: Container(
+          color: Colors.black,
+          height: 70,
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.search), text: 'Search'),
+              Tab(icon: Icon(Icons.home), text: 'New & Hot'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
